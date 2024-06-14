f45493a9f9c884f04b2488ccf35ee1dbdc6a2b6f
muhammadtalha <mtalhatariqx@gmail.com>
2024-06-14 01:21:51 -0700

    done

--- app/components/Chefs/page.jsx
+++ app/components/Chefs/page.jsx
@@ -35,57 +35,55 @@ const team = [
 
 const page = () => {
   return (
-    <>
-      <div
-        className="text-white bg-neutral-900 w-full min-h-screen  py-32 p-4"
-        id="Chefs"
-      >
-        <div>
-          <h2 className="text-3xl text-green-500 pb-4 text-center">Chefs</h2>
-          <h1 className="text-4xl font-normal leading-snug pb-6 text-center">
-            Meet Our Experts
-          </h1>
-        </div>
-        <div className="flex flex-wrap py-6 justify-center">
-          {team.map((item, index) => (
-            <Card
-              key={index}
-              className="bg-neutral-950 mx-3 rounded-xl hover:-translate-y-2 duration-1000 "
-              sx={{ maxWidth: 300 }}
-            >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="100"
-                  image={item.img}
-                  alt="green iguana"
-                  className="z-10"
-                />
-                <CardContent className="border-b-4 border-green-500">
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    className="text-white font-serif text-center"
-                  >
-                    {item.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    className="text-green-500 text-center"
-                  >
-                    {item.position}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
-        </div>
-        
-        <Reservation />
+    <div
+      className="text-white bg-neutral-900 w-full min-h-screen  py-32 p-4"
+      id="Chefs"
+    >
+      <div>
+        <h2 className="text-3xl text-green-500 pb-4 text-center">Chefs</h2>
+        <h1 className="text-4xl font-normal leading-snug pb-6 text-center">
+          Meet Our Experts
+        </h1>
       </div>
-    </>
+      <div className="flex flex-wrap py-6 justify-center">
+        {team.map((item, index) => (
+          <Card
+            key={index}
+            className="bg-neutral-950 mx-3 rounded-xl hover:-translate-y-2 duration-1000 "
+            sx={{ maxWidth: 300 }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="100"
+                image={item.img}
+                alt="green iguana"
+                className="z-10"
+              />
+              <CardContent className="border-b-4 border-green-500">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="text-white font-serif text-center"
+                >
+                  {item.name}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className="text-green-500 text-center"
+                >
+                  {item.position}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
+
+      <Reservation />
+    </div>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ import About from "./components/About/page";
 import Feature from "./components/Feature/page";
 import Menu from "./components/Menu/page";
 import Gallery from "./components/Gallery/page";
-import  Offer from "./components/offer/page"
-import Chefs from "./components/Chefs/page"
-import Testimonials from "./components/Testimonials/page"
-import Footer from "./components/Footer/page"
+import  Offer from "./components/offer/page";
+import Chefs from "./components/Chefs/page";
+import Testimonials from "./components/Testimonials/page";
+import Footer from "./components/Footer/page";
 const page = () => {
   return (
     <div>
